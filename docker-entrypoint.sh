fab1a8d62e5efe19232233ede89ea81ea65ebb13
Rafal Pieniazek <rafal@pieniazek.nl>
2017-10-19 12:48:07 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+set -e
+
+#Make the log file & change log file permission
+touch /var/log/haproxy/haproxy.log
+chmod 644 /var/log/haproxy/haproxy.log
+
 # Make sure service is running
 service rsyslog start
-
-# Touch the log file so we can tail on it
-touch /var/log/haproxy.log
 
 # Throw the log to output
 tail -f /var/log/haproxy.log &
 
 # Start haproxy
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+
